34eeea80fbccacb0e1d34ca3f1625a91de125e18
Shayajs <espinar.lucas0@gmail.com>
2026-05-17 14:16:26 +0200

    fix(prospects): remplace @php(...) par bloc @php/@endphp pour éviter ParseError Blade

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -293,7 +293,9 @@
                                 'icon'   => '<path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="16" y1="13" x2="8" y2="13"/><line x1="16" y1="17" x2="8" y2="17"/><line x1="10" y1="9" x2="8" y2="9"/>',
                             ])
 
-                            @php($prospectsUrl = \App\Support\PortalUrls::prospectsUrl())
+                            @php
+                                $prospectsUrl = \App\Support\PortalUrls::prospectsUrl();
+                            @endphp
                             @if ($prospectsUrl !== '')
                                 @include('layouts.partials.nav-section', ['label' => 'Prospection B2B'])
                                 @include('layouts.partials.nav-item', [
